d26aa58b484d260dcabaac05b27ef3f8518ac0af
Todd Phillips <37921585+tphillips2973@users.noreply.github.com>
2019-02-03 19:20:39 -0500

    Initial Solution Compile

---
+++ TPhillipsAssignment1/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPhillipsAssignment1
+{
+    class Circle
+    {
+
+        public int GetRadius()
+        {
+
+            return 1;
+        }
+
+        public void SetRadius(int radius)
+        {
+
+        }
+
+        public double GetCircumference()
+        {
+
+        }
+
+        public double GetArea()
+        {
+
+        }
+    }
+}
